774cb73dd7fd4248883eec25d776435674906888
Emirhan Yağcı <66957950+emirhan-yagci@users.noreply.github.com>
2022-07-15 02:49:01 +0300

    responsive bug fix

--- src/components/sectionContainer.vue
+++ src/components/sectionContainer.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="text-white flex items-center justify-center">
-        <div class="z-[999999999] h-[600px] w-[70%] rounded-lg flex items-center  bg-[#1D1933] xl:w-[75%] xl:h-[650px] sm:w-[90%]">
+        <div class="z-[999999999] h-[650px] w-[70%] rounded-lg flex items-center  bg-[#1D1933] xl:w-[75%] xl:h-[650px] sm:w-[90%]">
            <router-view></router-view>
         </div>
     </div>
