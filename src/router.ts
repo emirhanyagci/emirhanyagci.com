cc57bb8ad325281931905c2284ef4362b87d887f
Emirhan Yağcı <emirhan.yac2@gmail.com>
2023-06-23 13:13:45 +0300

    contact section removed from router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,19 @@
-import {createRouter,createWebHistory} from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
 import theAbout from "./components/theAbout.vue"
 import theExpert from "./components/theExpert.vue"
 import theBlog from "./components/theBlog.vue"
-import theContact from "./components/theContact.vue"
 
-const routes:any = [
-    {path:"/",redirect:"/about"},
-    {path:"/about",component:theAbout},
-    {path:"/skill",component:theExpert},
-    {path:"/blog",component:theBlog},
-    {path:"/contact",component:theContact},
+const routes: any = [
+    { path: "/", redirect: "/about" },
+    { path: "/about", component: theAbout },
+    { path: "/skill", component: theExpert },
+    { path: "/blog", component: theBlog },
 
 
 ]
 const router = createRouter({
-    history:createWebHistory(),
+    history: createWebHistory(),
     routes,
-  
+
 })
 export default router;
